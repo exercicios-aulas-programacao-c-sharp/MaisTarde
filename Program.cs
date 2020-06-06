135e22218ee47c6fc8fccbc83c0b1c7c4c518f6d
diego_neri <diego@diegoneri.com>
2020-06-05 22:01:23 -0300

    Finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaisTarde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataHoraAtual = DateTime.Now;
+            TimeSpan seisHorasEMeia = new TimeSpan(6,30,00);
+
+            DateTime dataHoraApos = dataHoraAtual.AddHours(6).AddMinutes(30).Date;
+            DateTime dataHoraAposComIntervalo = dataHoraAtual + seisHorasEMeia;
+
+            Console.WriteLine($"Agora {dataHoraAtual.ToShortTimeString()}, mais tarde {dataHoraApos.ToShortTimeString()}.");          
+            Console.WriteLine($"Agora {dataHoraAtual.ToShortTimeString()}, mais tarde, com intervalo {dataHoraAposComIntervalo.ToShortTimeString()}.");
+        }
+    }
+}
